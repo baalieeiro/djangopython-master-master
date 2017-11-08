c6dec034db6a153e215f508a8bed89a9fd1ac028
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-11-08 12:46:06 -0200

    Rodrigo

--- app/forms.py
+++ app/forms.py
@@ -4,12 +4,10 @@ from django import forms
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(max_length=200, label=("Login: "), required=True)
     password = forms.CharField(widget=forms.PasswordInput(attrs=dict(required=True, max_length=30)), label=("Senha: "))
-
 class CursoForms(forms.Form):
     nome = forms.CharField(max_length=100, label=("Nome: "), error_messages={'required': 'Nome'})
     periodo = forms.CharField(max_length=50, label=("Periodo: "), error_messages={'required': 'Periodo'})
     instituicao = forms.CharField(max_length=200, label=("Instituição: "), error_messages={'required': 'Instituição'})
-
 class AlunoForms(forms.Form):
     nome = forms.CharField(max_length=200, label=("Nome: "), error_messages={'required': 'Nome'})
     ra = forms.CharField(max_length=10, label=("Ra: "), error_messages={'required': 'Ra'})
@@ -21,7 +19,6 @@ class AlunoForms(forms.Form):
     estado = forms.CharField(max_length=10, label=("Estado: "), error_messages={'required': 'Estado'})
     telefone = forms.CharField(max_length=15, label=("Telefone: "), error_messages={'required': 'Telefone'})
     celular = forms.CharField(max_length=15, label=("Celular: "), error_messages={'required': 'Celular'})
-
 class CandidatoForms(forms.Form):
     nome = forms.CharField(max_length=200, label=("Nome: "), error_messages={'required': 'Nome'})
     curso = forms.CharField(max_length=100, label=("Curso: "), error_messages={'required': 'Curso'})
@@ -32,7 +29,6 @@ class CandidatoForms(forms.Form):
     estado = forms.CharField(max_length=10, label=("Estado: "), error_messages={'required': 'Estado'})
     telefone = forms.CharField(max_length=15, label=("Telefone: "), error_messages={'required': 'Telefone'})
     celular = forms.CharField(max_length=15, label=("Celular: "), error_messages={'required': 'Celular'})
-
 class ColaboradorForms(forms.Form):
     nome = forms.CharField(max_length=200, label=("Nome: "), error_messages={'required': 'Nome'})
     cargo = forms.CharField(max_length=100, label=("Cargo: "), error_messages={'required': 'Cargo'})
