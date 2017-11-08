82362a9a8bb51172f7d5f5cfc20307a5715a6507
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-11-08 13:30:47 -0200

    Rodrigo

--- app/models.py
+++ app/models.py
@@ -1,10 +1,5 @@
-"""
-Definition of models.
-"""
-
 from django.db import models
 
-# Create your models here.
 class Curso(models.Model):
     nome = models.CharField(max_length=200)
     periodo = models.CharField(max_length=50)
