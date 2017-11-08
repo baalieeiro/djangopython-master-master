1dd6cdfd032c09d84be9a27ceb26874e0f7eda3f
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-11-08 13:46:29 -0200

    Rodrigo

--- django_get_started/settings.py
+++ django_get_started/settings.py
@@ -124,10 +124,7 @@ ROOT_URLCONF = 'django_get_started.urls'
 WSGI_APPLICATION = 'django_get_started.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or
-    # "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    BASE_DIR + '/templates'
 )
 
 INSTALLED_APPS = (
