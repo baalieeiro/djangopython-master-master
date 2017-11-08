8ec9f5ec9596818a3dbdfc25fc6c6036cce39fe2
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-11-08 12:43:15 -0200

    Rodrigo

--- app/views.py
+++ app/views.py
@@ -179,3 +179,17 @@ def novo_usuario(request):
             'year':datetime.now().year,
         })
     )
+'''
+def novo_usuario(request):
+    assert isinstance(request, HttpRequest)
+    return render(
+        request,
+        'app/novo_usuario.html',
+        context_instance = RequestContext(request,
+        {
+            'title':'Novo usuario',
+            'message':'Novo usuario',
+            'year':datetime.now().year,
+        })
+    )
+'''
