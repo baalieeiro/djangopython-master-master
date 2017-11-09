bd72093ff4c4fe04939c7ad8b0a858ac765ad867
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-11-09 14:48:40 -0200

    Rodrigo

--- app/forms.py
+++ app/forms.py
@@ -41,7 +41,7 @@ class AlunoForm(forms.ModelForm):
     max_length=30,
     widget=forms.TextInput(
         attrs={
-            'style': 'border-color: blue;',
+            'style': 'border-color: green;',
             'placeholder': 'Write your name here'
         }
     )
