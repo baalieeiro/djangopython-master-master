30838b2cc714033a241012f01c94820123cd0d0f
Rodrigo Furlaneti <rodrigofurlaneti31@hotmail.com>
2017-11-10 10:59:25 -0200

    R

--- app/models.py
+++ app/models.py
@@ -17,6 +17,12 @@ class Aluno(models.Model):
     telefone = models.CharField(max_length=15)
     celular = models.CharField(max_length=15)
 
+    def __unicode__(self):
+        return self.nome
+
+    def get_absolute_url(self):
+        return reverse('editar_aluno', kwargs={'pk': self.pk})
+
 class Candidato(models.Model):
     nome = models.CharField(max_length=100)
     curso = models.CharField(max_length=100)
